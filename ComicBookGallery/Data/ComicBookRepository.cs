7bfd5d6a14d8dd0ec1e5938fd8a7252ec31b211f
Michael Lauri <pjuker@gmail.com>
2017-09-08 22:28:53 +0200

    Added Comic Books List view

--- ComicBookGallery/Data/ComicBookRepository.cs
+++ ComicBookGallery/Data/ComicBookRepository.cs
@@ -60,6 +60,11 @@ namespace ComicBookGallery.Data
             }
     };
 
+        public ComicBook[] GetcomicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
